71fcb3a0354dfee46e6f67dc4b53bfc5483d8cba
osmarks <osmarks@protonmail.com>
2018-04-30 20:22:54 +0100

    Fix recipes for 16-slot grid, add more recipes

--- patterns.lua
+++ patterns.lua
@@ -1,8 +1,15 @@
 local st = "minecraft:stone"
 local stbr = "minecraft:stonebrick"
 local stbrsl = "minecraft:stone_slab:5"
+local rs = "minecraft:redstone"
+local rsbl = "minecraft:redstone_block"
+
+local function block9(x)
+    return {x, x, x, [5] = x, [6] = x, [7] = x, [9] = x, [10] = x, [11] = x}
+end
 
 return {
-    [stbr] = {st, st, [4] = st, [5] = st, qty = 4},
-    [stbrsl] = {stbr, stbr, stbr, qty = 6}
+    [stbr] = {st, st, [5] = st, [6] = st, qty = 4},
+    [stbrsl] = {stbr, stbr, stbr, qty = 6},
+    [rsbl] = block9(rs)
 }
